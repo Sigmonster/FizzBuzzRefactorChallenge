8d0a2cea5cf679e5ff81c67aad0a85ae2f963d7a
AJ Sigmon <alexandersigmon@gmail.com>
2016-07-07 15:10:04 -0400

    finished with the prompt for integer addtion

--- js/jsfizzbuzz.js
+++ js/jsfizzbuzz.js
@@ -6,10 +6,17 @@ document.write(mytest);
 */
 
 var numbers = [];
+var numfactor=.5;
+
+do{
+numfactor = prompt("Please enter a number, no decimals please.");
+}
+while (numfactor % 1 != 0);
+numfactor = 0+parseInt(numfactor);
 
 for (var i =0; i<100; i++){
 	numbers[i] = i + 1;
-	if (0==(numbers[i]%3)) {
+	if (0==(numbers[i]%numfactor)) {
 		if(0==(numbers[i]%5)){
 		numbers[i] ="FizzBuzz";
 		}
